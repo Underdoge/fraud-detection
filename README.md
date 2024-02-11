11350875c049049d7d52385d60ebfe6a51913c79
Eduardo Chapa <eduardo.chapa@gmail.com>
2024-02-11 03:05:02 -0600

    Error Analysis and metaflow pipeline

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Proyecto Final para el Bootcamp de Machine Learning de Codigo Facilito 2023
 
+
+# Installation
+Open up a Terminal (macOS/Linux) or PowerShell (Windows) and enter the following commands:
+### Cloning the repository
+```sh
+git clone https://github.com/underdoge/proyecto-final-bcml
+
+cd proyect-final-bcml
+```
+### Creating the virtual environment
+```sh
+python -m venv .venv
+```
+### Activating the virtual environment
+```sh
+source .venv/bin/activate
+```
+### Installing on macOS / Linux
+```sh
+pip install -r requirements_macos_linux.txt
+```
+### How to run metaflow pipeline
+```sh
+python3 fraud_detection_flow.py run --source-file data/credit_card_transactions-ibm_v2.csv
+```
---
+++ feature_pipeline.py
@@ -0,0 +1,76 @@
+""" Feature engineering and training pipeline. """
+
+def build_pipeline():
+    """ Build pipeline with feature encoders and model. """
+    import mlflow
+    from sklearn.compose import ColumnTransformer  # noqa: E402
+    from sklearn.ensemble import RandomForestClassifier  # noqa: E402
+    from sklearn.pipeline import FeatureUnion, Pipeline  # noqa: E402
+    from sklearn.preprocessing import RobustScaler, TargetEncoder  # noqa: E402
+    # Target encoder
+    internal_target_encoding = TargetEncoder(smooth="auto")
+    columns_to_encode = [
+        "Card",
+        "Merchant Name",
+        "Merchant State",
+        "Merchant City",
+        "Zip",
+        "MCC",
+        "Errors?",
+        "Hour",
+        "Minute",
+        "Use Chip"
+    ]
+
+    mlflow.log_params("target_encoded_columns", columns_to_encode)
+    encoder_params = internal_target_encoding.get_params()
+    mlflow.log_params(
+        {f"encoder__{key}": value for key, value in encoder_params.items()})
+
+    target_encoding = ColumnTransformer([
+        (
+            'target_encode',
+            internal_target_encoding,
+            columns_to_encode
+        )
+    ])
+
+    # Scaler
+    internal_scaler = RobustScaler()
+    columns_to_scale = ["Amount"]
+
+    mlflow.log_params("scaled_columns", columns_to_scale)
+    scaler_params = internal_scaler.get_params()
+    mlflow.log_params(
+        {f"scaler__{key}": value for key, value in scaler_params.items()})
+
+    scaler = ColumnTransformer([
+        ("scaler", internal_scaler, columns_to_scale)
+    ])
+
+    # Full pipeline
+    feature_engineering_pipeline  = Pipeline([
+        (
+            "features",
+            FeatureUnion([
+                ('categories', target_encoding),
+                ('scaled', scaler)
+            ])
+        )
+    ])
+
+    # Machine learning model
+    model = RandomForestClassifier(n_estimators=10, verbose=1, n_jobs=10)
+
+    model_params = model.get_params()
+    mlflow.log_params({
+        f"model__{key}": value for key, value in model_params.items()
+    })
+
+    # Full pipeline
+    final_pipeline = Pipeline([
+        ("feature_engineering", feature_engineering_pipeline),
+        ("model", model)
+    ])
+
+    return final_pipeline
